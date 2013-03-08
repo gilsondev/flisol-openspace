b63a884dd5f61aec5f7e14225a250957a6104254
gilsondev <contato@gilsondev.com>
2013-03-07 22:43:35 -0300

    Inserindo URL

--- openspace/urls.py
+++ openspace/urls.py
@@ -8,15 +8,9 @@ from django.views.generic.simple import direct_to_template
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'openspace.views.home', name='home'),
+    url('^canal-teste/$', 'openspace.core.views.channel_detail', name='channel_detail'),
     url(r'^$', direct_to_template, {'template': 'base.html'}, name='home'),
-    # url(r'^openspace/', include('openspace.foo.urls')),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
 )
 
