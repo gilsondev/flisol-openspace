5a69b0c4e92696dd0557398cb38fb01b003822dc
gilsondev <contato@gilsondev.com>
2013-03-07 22:42:00 -0300

    Criando pagina para 404 e 500

--- openspace/settings.py
+++ openspace/settings.py
@@ -75,7 +75,7 @@ STATICFILES_DIRS = (
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+    #'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
 # Make this unique, and don't share it with anybody.
@@ -85,7 +85,7 @@ SECRET_KEY = 'mpf7l$q5tcw@+!81jkow-xong6wt6xm-(wh!9x_pc_9z6+2z^i'
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
+    #django.template.loaders.eggs.Loader',
 )
 
 MIDDLEWARE_CLASSES = (
