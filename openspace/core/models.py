cf9efa774e13283e939e893a3fe3420137fdb96c
gilsondev <contato@gilsondev.com>
2013-03-20 20:35:45 -0300

    Inserindo help_text para campo slug

--- openspace/core/models.py
+++ openspace/core/models.py
@@ -7,7 +7,12 @@ from django.utils.translation import ugettext_lazy as _
 class Channel(models.Model):
     name = models.CharField(_(u"Nome do Canal"), max_length=50)
     description = models.TextField(_(u"Descrição"), blank=True)
-    slug = models.SlugField(_(u"Endereço"), max_length=50)
+    slug = models.SlugField(_(u"Endereço"), max_length=50,
+                            help_text=_(u"""
+                                        Será a URL do seu canal.
+                                        Exemplo:
+                                        <strong>http://openspace.flisol.net/canais/
+                                        [seu endereço]</strong>"""))
 
     class Meta:
         verbose_name = u"canal"
