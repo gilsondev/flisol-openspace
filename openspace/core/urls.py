f76db0e2548d5b5d2e751d82df7863a6323870e6
gilsondev <contato@gilsondev.com>
2013-03-30 14:05:44 -0300

    Corrigindo URL e arrumando listagem de canais

--- openspace/core/urls.py
+++ openspace/core/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('openspace.core.views',
     url(r'^$', 'channel_list', name='list'),
-    url('^(?P<slug>[-\w]+)/$', 'channel_detail', name='details'),
+    url(r'^canais/(?P<slug>[-\w]+)/$', 'channel_detail', name='details'),
 )
